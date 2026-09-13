186a6526c8935d89f57980961cd205aed1a1c2ad
BitAn0n <tejirnxi@gmail.com>
2026-09-13 11:13:16 +0330

    Fix missing volume units in service notifications cron

--- cronbot/NoticationsService.php
+++ cronbot/NoticationsService.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../config.php';
 require_once __DIR__ . '/../botapi.php';
 require_once __DIR__ . '/../panels.php';
 require_once __DIR__ . '/../function.php';
+$textbotlang = languagechange();
 class ServiceMonitor
 {
     private $Panel;
